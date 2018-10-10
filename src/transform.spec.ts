62b3ae3a108f6b106838af03e2d48c8214bdb507
Caleb Froese <cfroese@nextfaze.com>
2018-10-10 21:20:13 +1030

    Add definition functionality

--- src/transform.spec.ts
+++ src/transform.spec.ts
@@ -5,15 +5,75 @@ describe('Transform', () => {
   it('transforms the template', done => {
     const event = {
       region: 'us-east-1',
-      accountId: '$ACCOUNT_ID',
-      fragment: {},
-      transformId: '$TRANSFORM_ID',
+      accountId: 'XXXYYYZZZ',
+      fragment: {
+        AWSTemplateFormatVersion: '2010-09-09',
+        Resources: {
+          ExampleStateMachineWithOutDef: {
+            Type: 'AWS::StepFunctions::StateMachine',
+            Properties: {
+              DefinitionString: 'ABC',
+            },
+          },
+          ExampleStateMachine: {
+            Type: 'AWS::StepFunctions::StateMachine',
+            Properties: {
+              Definition: {
+                Comment: 'PIRSA Prawn Report Report State Machine',
+                StartAt: 'Test',
+                States: [
+                  {
+                    Test: {
+                      Type: 'Pass',
+                      Result: 'Done',
+                      End: true,
+                    },
+                  },
+                ],
+              },
+            },
+          },
+          ExampleStateMachineRole: {
+            Type: 'AWS::Other::Resource',
+            Properties: {
+              Amount: 5,
+            },
+          },
+        },
+      },
+      transformId: 'XXX::ParseDefinitionString',
       params: {},
-      requestId: '$REQUEST_ID',
-      templateParameterValues: {},
+      requestId: '000-111-222',
     };
     transform(event, null, (err: any, data: any) => {
-      expect(data).to.deep.eq({});
+      expect(data).to.deep.eq({
+        status: 'success',
+        requestId: '000-111-222',
+        fragment: {
+          AWSTemplateFormatVersion: '2010-09-09',
+          Resources: {
+            ExampleStateMachineWithOutDef: {
+              Type: 'AWS::StepFunctions::StateMachine',
+              Properties: {
+                DefinitionString: 'ABC',
+              },
+            },
+            ExampleStateMachine: {
+              Type: 'AWS::StepFunctions::StateMachine',
+              Properties: {
+                DefinitionString:
+                  '{"Comment":"PIRSA Prawn Report Report State Machine","StartAt":"Test","States":[{"Test":{"Type":"Pass","Result":"Done","End":true}}]}',
+              },
+            },
+            ExampleStateMachineRole: {
+              Type: 'AWS::Other::Resource',
+              Properties: {
+                Amount: 5,
+              },
+            },
+          },
+        },
+      });
       expect(err).null;
       done();
     });
--- src/transform.ts
+++ src/transform.ts
@@ -1,9 +1,27 @@
 export function transform(event: any, ctx: any, callback: Function) {
-  console.log(event);
-  console.log(JSON.stringify(event));
+  // Find instances of StateMachines
+  const StateMachines = Object.keys(event.fragment.Resources)
+    .map(name => ({ name, resource: event.fragment.Resources[name] }))
+    // only care about state machines
+    .filter(
+      ({ resource }) => resource.Type === 'AWS::StepFunctions::StateMachine'
+    )
+    // only care about those with Definition
+    .filter(({ resource }) => !!resource.Properties.Definition);
+
+  // Parse each of the definitions
+  StateMachines.forEach(stateMachine => {
+    event.fragment.Resources[
+      stateMachine.name
+    ].Properties.DefinitionString = JSON.stringify(
+      stateMachine.resource.Properties.Definition
+    );
+    delete event.fragment.Resources[stateMachine.name].Properties.Definition;
+  });
+
   callback(null, {
     status: 'success',
     requestId: event.requestId,
-    fragment: 'test',
+    fragment: event.fragment,
   });
 }
